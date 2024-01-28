78bf8f72efd65c00d84e82f89fe242af8380f9ae
john-abou <abounassar.john@gmail.com>
2024-01-28 13:04:03 -0500

    Feat: Home page added to app set up. Home page state vars and timer start/stop function defined.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Home from './home'
 
 export default function App() {
     return(
-        <div></div>
+        <div>
+            <Home/>
+        </div>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import StopWatch from '../components/StopWatch';
 import StopWatchButton from '../components/StopWatchButton';
 
+
 const Stopwatch: React.FC = () => {
   // Define state variables for if the clock is running, the current time and the laps
+  const [timerOn, setTimerOn] = useState(false);
+  const [time, setTime] = useState(0)
+  const [laps, setLaps] = useState<number[]>([])
 
   // Create functions to handle start/stop of the watch, laps, reset timer
   // Start/stop function should reverse the boolean value of the state var
+  const handleStartStop = () => {
+    setTimerOn(!timerOn)
+  }
   // Lap function should update state var by creating a new array with the previous lap time and restart the timer so the current lap starts from 0
   // Reset function should empty the laps array, set time to 0 and stop the watch
 
@@ -17,6 +24,6 @@ const Stopwatch: React.FC = () => {
       {/* The display and buttons should be put here, pass functions and state vars as props. */}
     </div>
   )
-    
-  
 }
+
+export default StopWatch;
